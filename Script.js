356a320899580104dfa8c5b72c9b4929204eb069
Ethan85-lobby <ethan.clain@icloud.com>
2025-11-25 14:31:44 +0000

    feat: correction de ligne de code

--- Script.js
+++ Script.js
@@ -1,2 +1,12 @@
-<script src="script.js"></script>
 console.log("le script est chargé !")
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <script src="script.js"></script>   
+</body>
+</html>
